63111c6b1b27224f1a0f3776e5ec7916568ab618
Carsten Knoll <carsten.knoll@posteo.de>
2022-05-31 19:43:34 +0200

    add first rudimentary unittest

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,7 +1,16 @@
 import unittest
+import sys
+import os
+from os.path import join as pjoin
+# noinspection PyUnresolvedReferences
 from ipydex import IPS, activate_ips_on_exception
+import pykerl as pk
 
 activate_ips_on_exception()
+
+current_dir = os.path.dirname(os.path.abspath(sys.modules.get(__name__).__file__))
+TEST_DATA_PATH = pjoin(current_dir, "test_data")
+
 
 # noinspection PyPep8Naming
 class TestCore(unittest.TestCase):
@@ -10,4 +19,4 @@ class TestCore(unittest.TestCase):
 
     # mark tests which only work for the "old core"
     def test_core1(self):
-        pass
+        m = pk.Manager(pjoin(TEST_DATA_PATH, "test1.yml"))

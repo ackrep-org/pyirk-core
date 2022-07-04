d4eb8a0ec5ed0b993af01c86352ec4d4ba20e74b
Carsten Knoll <carsten.knoll@posteo.de>
2022-07-05 01:16:30 +0200

    allow to omit reloading the data-modules

--- src/pyerk/erkloader.py
+++ src/pyerk/erkloader.py
@@ -7,12 +7,16 @@ from ipydex import IPS, activate_ips_on_exception
 activate_ips_on_exception()
 
 
-def load_mod_from_path(modpath: str, modname=None, allow_reload=True):
+def load_mod_from_path(modpath: str, modname=None, allow_reload=True, omit_reload=False):
     if modname is None:
         raise NotImplementedError
 
     modpath = os.path.abspath(modpath)
     old_mod_id = pyerk.ds.mod_path_mapping.b.get(modpath)
+
+    if omit_reload and old_mod_id:
+        return
+
     if allow_reload and old_mod_id:
         pyerk.unload_mod(old_mod_id)
 

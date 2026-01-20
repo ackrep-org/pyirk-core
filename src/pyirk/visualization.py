2f12c932bd1d988bea5cc2f3bbe2200859174cc6
Carsten Knoll <carsten.knoll@posteo.de>
2026-01-20 16:25:35 +0100

    fix (visualization.py): fix nxv issue with monkey-patching

--- src/pyirk/visualization.py
+++ src/pyirk/visualization.py
@@ -11,6 +11,15 @@ import re
 import time
 
 import networkx as nx
+
+# monkey-patch workaround because nxv currently is broken for new networkx releases
+
+if not hasattr(nx, "OrderedGraph"):
+    nx.OrderedGraph = nx.Graph
+    nx.OrderedDiGraph = nx.DiGraph
+    nx.OrderedMultiGraph = nx.MultiGraph
+    nx.OrderedMultiDiGraph = nx.MultiDiGraph
+
 import nxv  # for graphviz visualization of networkx graphs
 
 # prevent deprecation warning because nxv is not as up-to-date as nx (of which it depends)
